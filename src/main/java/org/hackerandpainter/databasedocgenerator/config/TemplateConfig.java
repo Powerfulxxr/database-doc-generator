b480dca631537433cfdd9196f508926c110532e7
XUXIAORONG <2235157443@qq.com>
2020-12-23 10:12:17 +0800

    修改application-common.properties位置

--- src/main/java/org/hackerandpainter/databasedocgenerator/config/TemplateConfig.java
+++ src/main/java/org/hackerandpainter/databasedocgenerator/config/TemplateConfig.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
  * @author xxr12
  */
 @Component
-@PropertySource("classpath:application-common.properties")
+@PropertySource("classpath:common/application-common.properties")
 @ConfigurationProperties(prefix = "shelf")
 public class TemplateConfig {
 
