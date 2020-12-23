8872c8d73bae6708dbe86cb4d1633f0105b3e4c1
XUXIAORONG <2235157443@qq.com>
2020-12-23 16:58:49 +0800

    @Profile("window")

--- src/main/java/org/hackerandpainter/databasedocgenerator/config/GuideConfig.java
+++ src/main/java/org/hackerandpainter/databasedocgenerator/config/GuideConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ import java.io.IOException;
 /**
  * @author xxr12
  */
+@Profile("window")
 @Slf4j
 @Configuration
 public class GuideConfig {
